58dc741be9e888308c7417e87ce1bfd7bd284d52
greicybenitez <greicy.azevedo@estudante.ifms.edu.br>
2022-03-08 17:58:34 -0300

    criando a função limpar

--- Simulador_Loteria-master/funcoes.js
+++ Simulador_Loteria-master/funcoes.js
@@ -18,12 +18,13 @@ function getNum(valor, pos){
   valor = Number(valor)
    if(valor>60){
      alert("O valor é maior que 60")
-
+     limpar(pos)
    } else if(valor <=0){
      alert("O valor não pode ser menor que 0")
+     limpar(pos)
    }else if(numDigitados.includes(valor)) {
      alert("O valor já foi digitado!")
-
+         limpar(pos)
    }
 
    else{
@@ -31,6 +32,10 @@ function getNum(valor, pos){
    }
 
 }
+   function limpar(pos){
+     document.getElementById(`num${pos}`).value = ""
+
+   }
 function verificaAcertos(){
   let cont = 0
   numDigitados.forEach(function(valor,index){
