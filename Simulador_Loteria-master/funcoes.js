13fadb6d99f1c7821b24aa329dea47c004386349
greicybenitez <greicy.azevedo@estudante.ifms.edu.br>
2022-03-08 17:32:21 -0300

    final das funções importantes

---
+++ Simulador_Loteria-master/funcoes.js
@@ -0,0 +1,43 @@
+var numSort = []
+var numDigitados = []
+
+function sorteioNum(){
+     numSort = []
+     do{
+        let sort =  Math.ceil(Math.random() * 60)
+        if(!numSort.includes(sort))
+       numSort.push(sort)
+     }while(numSort.length < 6);
+
+   document.getElementById("numsort").innerText = numSort
+        verificaAcertos()
+    }
+
+
+function getNum(valor, pos){
+  valor = Number(valor)
+   if(valor>60){
+     alert("O valor é maior que 60")
+
+   } else if(valor <=0){
+     alert("O valor não pode ser menor que 0")
+   }else if(numDigitados.includes(valor)) {
+     alert("O valor já foi digitado!")
+
+   }
+
+   else{
+     numDigitados[pos]=valor
+   }
+
+}
+function verificaAcertos(){
+  let cont = 0
+  numDigitados.forEach(function(valor,index){
+    if(numSort.includes(valor)){
+        cont = cont+1
+    }
+  })
+  document.getElementById("total").innerText = cont
+}
+
